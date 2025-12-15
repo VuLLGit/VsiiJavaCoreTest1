3003eb0f53c26e3caccae263723f1bc55cb2a954
VuLLGit <blockfull34@gmail.com>
2025-12-15 14:55:52 +0700

    modify main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,18 +15,38 @@ public class Main {
         list.add("VietSoftw@re@vsi-international.com");
         list.add("@@@gmail.com");
 
-        ArrayList<String> result = filter(list);
-
-        System.out.println(result);
+        System.out.println(filter(list));
     }
 
-    //phương thức filter xóa toàn bộ ký tự sau @
     private static ArrayList<String> filter(ArrayList<String> list) {
         ArrayList<String> result = new ArrayList<>();
         for(String s : list) {
+            // kiểm tra string có @ không
             if(s.contains("@")) {
-                String newString = s.substring(0, s.indexOf("@")+1) + "...";
-                result.add(newString);
+                int count = 0;
+                int firstAtIndex = -1;
+
+                //kiểm tra xem có dấu @ thứ 2 không và lấy ra vị trí @ thứ nhất
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '@') {
+                        count++;
+
+                        if (count == 1) {
+                            firstAtIndex = i;
+                        }
+
+                        if (count == 2) {
+                            String beforeSecondAt = s.substring(0, i).replace("@", "");
+                            result.add(beforeSecondAt + "@...");
+                            break;
+                        }
+                    }
+                }
+
+                // Nếu chỉ có 1 dấu @ thì lấy
+                if (count == 1) {
+                    result.add(s.substring(0, firstAtIndex) + "@...");
+                }
             }
         }
         return result;
