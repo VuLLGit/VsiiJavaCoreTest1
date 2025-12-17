66b242189078eed4adff9bbeeea0115ca08e1a5c
VuLLGit <blockfull34@gmail.com>
2025-12-17 14:18:40 +0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,30 +23,10 @@ public class Main {
         for(String s : list) {
             // kiểm tra string có @ không
             if(s.contains("@")) {
-                int count = 0;
-                int firstAtIndex = -1;
-
-                //kiểm tra xem có dấu @ thứ 2 không và lấy ra vị trí @ thứ nhất
-                for (int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) == '@') {
-                        count++;
-
-                        if (count == 1) {
-                            firstAtIndex = i;
-                        }
-
-                        if (count == 2) {
-                            String beforeSecondAt = s.substring(0, i).replace("@", "");
-                            result.add(beforeSecondAt + "@...");
-                            break;
-                        }
-                    }
-                }
-
-                // Nếu chỉ có 1 dấu @ thì lấy
-                if (count == 1) {
-                    result.add(s.substring(0, firstAtIndex) + "@...");
-                }
+                int lastAt = s.lastIndexOf("@");
+                // Phần trước @ cuối, loại bỏ mọi ký tự @ phía trước
+                String beforeAt = s.substring(0, lastAt).replace("@", "");
+                result.add(beforeAt + "@...");
             }
         }
         return result;
