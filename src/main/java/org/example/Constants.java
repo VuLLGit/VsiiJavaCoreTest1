552261d0b8d18aeb9a1c0b89bec7107a1a04e834
VuLLGit <blockfull34@gmail.com>
2025-12-18 14:20:24 +0700

    add constant

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String AFTER_STRING = "...";
+    public static final String AT = "@";
+}
--- src/main/java/org/example/FilterString.java
+++ src/main/java/org/example/FilterString.java
@@ -3,43 +3,36 @@ package org.example;
 import java.util.ArrayList;
 
 public class FilterString {
-    private static final String AFTERSTRING = "...";
-    private static final String AT = "@";
 
-    // sử dụng để lấy @ cuối cùng và loại bỏ toàn bộ ký tự phía sau nó thành "..."
     public ArrayList<String> process(ArrayList<String> list) {
-        // check null
         if (list == null) {
             throw new IllegalArgumentException("list must not be null");
         }
-        // khai báo
+
         ArrayList<String> result = new ArrayList<>();
         int lastAt;
         String tempString;
 
         for (String s : list) {
-            if (s.contains(AT)) {
-                lastAt = s.lastIndexOf(AT);
+            if (s.contains(Constants.AT)) {
+                lastAt = s.lastIndexOf(Constants.AT);
                 tempString = s.substring(0, lastAt + 1);
 
-                // loại bỏ mọi ký tự @ phía trước trừ phần cuối
                 for (int i = 0; i < tempString.length() - 1; i++) {
-                    // sau khi substring loại bỏ @ thì phải lùi một index để tránh bỏ qua các @ liền kề nhau
-                    if (String.valueOf(tempString.charAt(i)).equals(AT)) {
-                        tempString = tempString.substring(0, i) + tempString.substring(i + 1);
+                    if (String.valueOf(tempString.charAt(i)).equals(Constants.AT)) {
+                        tempString = tempString.substring(0, i)
+                                + tempString.substring(i + 1);
                         i--;
                     }
                 }
 
-                // kiểm tra xem sau @ còn ký tự không
                 if (lastAt == s.length() - 1) {
                     result.add(tempString);
                 } else {
-                    result.add(tempString + AFTERSTRING);
+                    result.add(tempString + Constants.AFTER_STRING);
                 }
             }
         }
         return result;
     }
-
 }
